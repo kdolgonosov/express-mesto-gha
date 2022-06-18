929702093d55735e90ab7cb351daabcc840e8cea
kdolgonosov <89645232+kdolgonosov@users.noreply.github.com>
2022-06-18 18:48:30 +0300

    refactor

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const auth = require('./middlewares/auth');
 const { urlRegex } = require('./constants/regex');
 const NotFoundError = require('./errors/notFoundError');
 const errorHandler = require('./middlewares/errorHandler');
-const router = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -18,38 +17,37 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-// app.post(
-//   '/signin',
-//   celebrate({
-//     body: Joi.object().keys({
-//       email: Joi.string().required().email(),
-//       password: Joi.string().required(),
-//     }),
-//   }),
-//   login,
-// );
-// app.post(
-//   '/signup',
-//   celebrate({
-//     body: Joi.object().keys({
-//       name: Joi.string().min(2).max(30),
-//       about: Joi.string().min(2).max(30),
-//       avatar: Joi.string().pattern(urlRegex),
-//       email: Joi.string().required().email(),
-//       password: Joi.string().required(),
-//     }),
-//   }),
-//   createUser,
-// );
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login,
+);
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().pattern(urlRegex),
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  createUser,
+);
 
-// app.use(auth);
-// app.use('/', require('./routes/users'));
-// app.use('/', require('./routes/cards'));
+app.use(auth);
+app.use('/', require('./routes/users'));
+app.use('/', require('./routes/cards'));
 
-// app.use('*', (req, res, next) => {
-//   next(new NotFoundError('Страница не найдена'));
-// });
-app.use(router);
+app.use('*', (req, res, next) => {
+  next(new NotFoundError('Страница не найдена'));
+});
 app.use(errors());
 app.use(errorHandler);
 
