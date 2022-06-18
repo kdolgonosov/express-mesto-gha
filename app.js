15dbfcda4c8f0948fb095cb4d5de988b7ae5a3f3
kdolgonosov <89645232+kdolgonosov@users.noreply.github.com>
2022-06-18 17:56:58 +0300

    refactor

--- app.js
+++ app.js
@@ -1,55 +1,60 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { errors } = require('celebrate');
+const { errors, Joi, celebrate } = require('celebrate');
+const helmet = require('helmet');
+const { login, createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
+const { urlRegex } = require('./constants/regex');
+const NotFoundError = require('./errors/notFoundError');
 const errorHandler = require('./middlewares/errorHandler');
-const router = require('./routes/index');
-// const { login, createUser } = require('./controllers/users');
-// const auth = require('./middlewares/auth');
-// const { urlRegex } = require('./constants/regex');
-// const NotFoundError = require('./errors/notFoundError');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.use(router);
-// app.post(
-//   '/signin',
-//   celebrate({
-//     body: Joi.object().keys({
-//       email: Joi.string().required().email(),
-//       password: Joi.string().required(),
-//     }),
-//   }),
-//   login,
-// );
-// app.post(
-//   '/signup',
-//   celebrate({
-//     body: Joi.object().keys({
-//       name: Joi.string().min(2).max(30),
-//       about: Joi.string().min(2).max(30),
-//       avatar: Joi.string().pattern(urlRegex),
-//       email: Joi.string().required().email(),
-//       password: Joi.string().required(),
-//     }),
-//   }),
-//   createUser,
-// );
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login,
+);
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().pattern(urlRegex),
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  createUser,
+);
 
-// app.use(auth);
-// app.use('/', require('./routes/users'));
-// app.use('/', require('./routes/cards'));
+app.use(auth);
+app.use('/', require('./routes/users'));
+app.use('/', require('./routes/cards'));
 
-// app.use('*', (req, res, next) => {
-//   next(new NotFoundError('Страница не найдена'));
-// });
+app.use('*', (req, res, next) => {
+  next(new NotFoundError('Страница не найдена'));
+});
 app.use(errors());
-app.use(errorHandler);
+// app.use((err, req, res, next) => {
+//   const { statusCode = 500, message } = err;
+//   res.status(statusCode).send({ message });
+//   next();
+// });
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порте: ${PORT}`);
